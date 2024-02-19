2b3a32b98d4749dc61529852da8d765a9a90f461
unknown <fatimazahraloukili02@gmail.com>
2024-02-19 09:38:25 +0100

    servelet

---
+++ my-servlet-app/src/main/java/com/hatim/app/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.hatim.app;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+public class HelloServlet  extends HttpServlet{
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        String yourName = req.getParameter("your_name");
+		out.write("<h1>GOOD MORNING " + yourName + "!<h1>");
+		out.close();
+    }
+
+}
